9df568deaad017c4c3823ac7ebce6c62ff7793a1
= <=>
2015-10-07 12:42:17 -0400

    Commit

---
+++ Pacman/Pacman.java
@@ -0,0 +1,15 @@
+package Pacman;
+public class Pacman 
+{	
+	public static void main(String[] args) 
+	{
+		//Display class_display = new Display();
+		Entity class_entity = new Entity();
+		
+		System.out.println("Hello World!");
+		//class_display.draw();
+		Entity.Enemy1();		
+		
+	}
+
+}
---
+++ Pacman/Pacman/Entity.java
@@ -0,0 +1,39 @@
+package Pacman;
+
+public class Entity 
+{
+	public char Symbol()
+	{
+		char border = 'x';
+		return border;
+	}
+	public char Symbol2()
+	{
+		char board = '*';
+		return board;
+	}
+	public char Enemy1()
+	{
+		char ghost = '@';
+		return ghost;
+	}
+	public char Enemy2()
+	{
+		char ghost2 = '#';
+		return ghost2;
+	}
+	public char Enemy3()
+	{
+		char ghost3 = '$';
+		return ghost3;
+	}
+	public char Enemy4()
+	{
+		char ghost4 = '%';
+		return ghost4;
+	}
+	public Entity() 
+	{
+		
+	}
+}
