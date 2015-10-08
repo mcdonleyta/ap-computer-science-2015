c31426168b743d5eaa730a00ded582b41bfe0bfb
= <=>
2015-10-08 09:14:05 -0400

    Fully working cypher.  Had to rewrite because I accidentally deleted my code.

--- cipher.java
+++ cipher.java
@@ -17,10 +17,34 @@ class Ciphering {
 	}
 
 	public String convert(String stringIn) {
-		char[] lettersIn = stringIn.tocharArray();
+		String stringOut = "";
+		char[] lettersIn = stringIn.toCharArray();
+		int place = 0;
 		for(int j = 0; j < lettersIn.length; j++) {
-			if()
+			if(lettersIn[j] != ' ') {
+				place = getIntPlace(lettersIn[j]);
+			}	
+			if(lettersIn[j] == alphabet.toCharArray()[place]) {
+				stringOut += Character.toString(altChars.toCharArray()[place]);
+			}
+			else if(lettersIn[j] == ' ') {
+				stringOut += " ";
+			}
 		}
+		return stringOut;
+	}
+
+}
+
+class cipher {
+
+	public static void main(String[] args) {
+		Ciphering cypher = new Ciphering();
+		Scanner getString  = new Scanner(System.in);
+		System.out.println("Enter some text, no caps");
+		String input = getString.nextLine();
+		String output = cypher.convert(input);
+		System.out.println(output);
 	}
 
 }
