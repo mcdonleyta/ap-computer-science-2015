fd328ff9e2a51e1e1640ab809fbc8b7df130e033
= <=>
2016-02-04 11:51:26 -0500

    updated_arraylist

--- labs/Sorting/src/Sorting/SortedArray.java
+++ labs/Sorting/src/Sorting/SortedArray.java
@@ -12,7 +12,7 @@ public class SortedArray {
     }
 
     public void add(Tomparable t) {
-        if (items.isEmpty() || t.compareTo(items.get(0)) < 0)
+        if (items.isEmpty())
             items.add(0, t);
         else {
             int size = items.size();
